454db8875a8ac86b809455df165193612d0f6f32
patil-suraj <surajp815@gmail.com>
2023-03-07 17:05:05 +0100

    position ids on device

--- muse/transformer.py
+++ muse/transformer.py
@@ -188,7 +188,7 @@ class Embed(nn.Module):
 
     def forward(self, input_ids):
         seq_length = input_ids.shape[-1]
-        position_ids = torch.arange(seq_length)[None, :]
+        position_ids = torch.arange(seq_length)[None, :].to(input_ids.device)
 
         word_embeddings = self.word_embeddings(input_ids)
         position_embeddings = self.position_embeddings(position_ids)
