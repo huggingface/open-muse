290f287f70c2684b292285eb4f0faa5e12d1796f
isamu-isozaki <isamu.website@gmail.com>
2023-05-08 07:34:02 -0400

    Adding discriminator warmup

--- training/train_vqgan.py
+++ training/train_vqgan.py
@@ -445,7 +445,7 @@ def main():
             pixel_values, _ = batch
             pixel_values = pixel_values.to(accelerator.device, non_blocking=True)
             data_time_m.update(time.time() - end)
-            generator_step = ((i // config.training.gradient_accumulation_steps) % 2) == 0
+            generator_step = ((i // config.training.gradient_accumulation_steps) % 2) == 0 and i > config.training.discriminator_warmup
             # TODO:
             # Add entropy to maximize codebook usage
             # Train Step
