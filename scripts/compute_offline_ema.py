35549f4346de8796536bda66653857944b8f3eea
patil-suraj <surajp815@gmail.com>
2023-05-24 01:05:47 +0530

    fix script

--- scripts/compute_offline_ema.py
+++ scripts/compute_offline_ema.py
@@ -16,8 +16,9 @@ def offline_ema(args):
     dirs = os.listdir(checkpoint_dir_path)
     dirs = [d for d in dirs if d.startswith("checkpoint")]
     dirs = sorted(dirs, key=lambda x: int(x.split("-")[1]))
+    dirs = [Path(checkpoint_dir_path) / dir_ for dir_ in dirs]
 
-    transformer_config = MaskGitTransformer.load_config(Path(dirs[0]) / "unwrapped_model")
+    transformer_config = MaskGitTransformer.load_config(dirs[0] / "unwrapped_model")
     if transformer_config["_class_name"] == "MaskGitTransformer":
         model_cls = MaskGitTransformer
     elif transformer_config["_class_name"] == "MaskGiTUViT":
@@ -27,15 +28,14 @@ def offline_ema(args):
     if torch.cuda.is_available():
         device = "cuda"
 
-    model = model_cls.from_pretrained(Path(dirs[0]) / "unwrapped_model").to(device)
-    ema_model = EMAModel(parameters=model.parameters(), ema_decay=ema_decay, update_every=checkpoint_interval).to(
-        device
-    )
+    model = model_cls.from_pretrained(dirs[0] / "unwrapped_model").to(device)
+    ema_model = EMAModel(parameters=model.parameters(), decay=ema_decay, update_every=checkpoint_interval)
+    ema_model.to(device)
 
-    end_step = int(dirs[-1].split("-")[1])
+    end_step = int(str(dirs[-1]).split("-")[-1])
     for step in range(0, end_step):
         if (step + 1) % checkpoint_interval == 0:
-            print(f"Loading checkpoint {step}...")
+            print(f"Loading checkpoint {step + 1}...")
             model.from_pretrained(Path(checkpoint_dir_path) / f"checkpoint-{step + 1}" / "unwrapped_model")
             model.to(device)
 
@@ -49,5 +49,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--checkpoint_dir_path", type=str, default=None, required=True)
     parser.add_argument("--ema_save_path", type=str, default=None, required=True)
-    parser.add_argument("--ema_decay", type=float, default=0.99)
+    parser.add_argument("--ema_decay", type=float, default=0.9999)
     parser.add_argument("--checkpoint_interval", type=int, default=1000)
+
+    args = parser.parse_args()
+    offline_ema(args)
