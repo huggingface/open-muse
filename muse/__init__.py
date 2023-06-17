3b7708d76d135777650cba1b4f86abd09f60da02
patil-suraj <surajp815@gmail.com>
2023-06-17 15:00:35 +0530

    add in int

--- muse/__init__.py
+++ muse/__init__.py
@@ -21,5 +21,6 @@ from .modeling_movq import MOVQ
 from .modeling_paella_vq import PaellaVQModel
 from .modeling_taming_vqgan import VQGANModel
 from .modeling_transformer import MaskGitTransformer, MaskGiTUViT
+from .modeling_ema import EMAModel
 from .pipeline_muse import PipelineMuse
 from .sampling import get_mask_chedule
