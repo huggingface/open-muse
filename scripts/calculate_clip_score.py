b6fa233e764ef0cd312ad332ac1a677326595f2f
isamu-isozaki <isamu.website@gmail.com>
2023-06-24 19:38:23 -0400

    Renamed clip score

--- scripts/calculate_clip_score.py
+++ scripts/calculate_clip_score.py
@@ -46,6 +46,10 @@ class DatasetWithGeneratedImages(Dataset):
         generated_image_inputs = self.processor(images=[generated_image], return_tensors="pt", padding=True)
         return text_inputs, real_image_inputs, generated_image_inputs
 
+class CLIPScoreOutput:
+    def __init__(self, clip_score=None, clip_image_score=None):
+        self.clip_score = clip_score
+        self.clip_image_score = clip_image_score
 
 def generate_and_save_images(args):
     """
@@ -102,8 +106,9 @@ def get_clip_scores(args, captions, real_image_names, generated_image_names):
     processor = AutoProcessor.from_pretrained(clip_model_name)
     dataset = DatasetWithGeneratedImages(real_images=real_image_names, generated_images=generated_image_names, captions=captions, processor=processor)
     dataloader = DataLoader(dataset, batch_size=args.batch_size)
-    clip_text_scores = []
-    clip_image_scores = []
+    clip_scores = []
+    if args.calculate_image_alignment:
+        clip_image_scores = []
     with torch.no_grad():
         for batch in tqdm(dataloader):
             if args.device == 'cpu':
@@ -112,27 +117,30 @@ def get_clip_scores(args, captions, real_image_names, generated_image_names):
                 text_inputs, real_inputs, generated_inputs = batch.to(args.device, dtype=torch.float16)
 
             text_embeds = clip_model.get_text_features(**text_inputs)
-            real_embeds = clip_model.get_image_features(**real_inputs)
             generated_embeds = clip_model.get_image_features(**generated_inputs)
 
             text_embeds = text_embeds / text_embeds.norm(p=2, dim=-1, keepdim=True)
-            real_embeds = real_embeds / real_embeds.norm(p=2, dim=-1, keepdim=True)
             generated_embeds = generated_embeds / generated_embeds.norm(p=2, dim=-1, keepdim=True)
             clip_text_alignment = torch.clip(torch.matmul(text_embeds, generated_embeds.t()) * w, 0)
-            clip_image_alignment = torch.clip(torch.matmul(real_embeds, generated_embeds.t()), 0)
             clip_text_alignment = clip_text_alignment.cpu().detach().numpy()
             clip_image_alignment = clip_image_alignment.cpu().detach().numpy()
-            clip_text_scores.append(clip_text_alignment)
-            clip_image_scores.append(clip_image_alignment)
-    clip_text_scores = np.mean(np.concatenate(clip_text_scores, axis=0))
-    clip_image_scores = np.mean(np.concatenate(clip_image_scores, axis=0))
-    return clip_text_scores, clip_image_scores
+            clip_scores.append(clip_text_alignment)
+            if args.calculate_image_alignment:
+                real_embeds = clip_model.get_image_features(**real_inputs)
+                real_embeds = real_embeds / real_embeds.norm(p=2, dim=-1, keepdim=True)
+                clip_image_alignment = torch.matmul(real_embeds, generated_embeds.t())
+                clip_image_scores.append(clip_image_alignment)
+    clip_score = np.mean(np.concatenate(clip_scores, axis=0))
+    if args.calculate_image_alignment:
+        clip_image_score = np.mean(np.concatenate(clip_image_score, axis=0))
+        return CLIPScoreOutput(clip_score=clip_score, clip_image_score=clip_image_score)
+    return CLIPScoreOutput(clip_score=clip_score)
 
 def main(args):
     captions, real_image_names, generated_image_names = generate_and_save_images(args)
     print("computing Image and Text CLIP Score")
-    clip_text_score, clip_image_score = get_clip_scores(args, captions, real_image_names, generated_image_names)
-    print(f"clip text score is {clip_text_score:.3f} clip image score is {clip_image_score:.3f}")
+    clip_score, clip_image_score = get_clip_scores(args, captions, real_image_names, generated_image_names)
+    print(f"clip score is {clip_score:.3f} clip image alignment score is {clip_image_score:.3f}")
 
 
 if __name__ == "__main__":
@@ -152,6 +160,7 @@ if __name__ == "__main__":
         ),
     )
     parser.add_argument("--enable_memory_efficient_attention", action='store_true')
+    parser.add_argument("--calculate_image_alignment", action='store_true')
     parser.add_argument("--save_path", type=str, required=True)
     parser.add_argument("--timesteps", type=int, default=12)
     parser.add_argument("--temperature", type=float, default=1.0)
