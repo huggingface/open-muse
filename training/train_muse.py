9f9b508fc8aa431e3e727ce9f03b5f349f5e7ac7
isamu-isozaki <isamu.website@gmail.com>
2023-08-12 13:42:24 -0400

    Fixed model size

--- training/train_muse.py
+++ training/train_muse.py
@@ -1007,7 +1007,7 @@ def get_model_size(model):
     mem_params = sum([param.nelement()*param.element_size() for param in model.parameters()])
     mem_bufs = sum([buf.nelement()*buf.element_size() for buf in model.buffers()])
     mem = mem_params + mem_bufs # in bytes
-    return mem // (1024**3)
+    return mem / (1024**3)
 
 if __name__ == "__main__":
     main()
