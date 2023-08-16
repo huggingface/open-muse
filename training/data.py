84cc014669b503516a7f9a5da00bebf380e64a98
Will Berman <WLBberman@gmail.com>
2023-08-16 00:55:06 +0000

    naming

--- training/data.py
+++ training/data.py
@@ -434,7 +434,7 @@ class Text2ImageDataset:
         require_marked_as_not_getty: bool = False,
         max_pnsfw: Optional[float] = None,
     ):
-        if train_shards_path_or_url == "m4_data":
+        if train_shards_path_or_url == "m4_shards":
             with open("./configs/m4_shards.yaml") as f:
                 train_shards_path_or_url = yaml.safe_load(f)
 
