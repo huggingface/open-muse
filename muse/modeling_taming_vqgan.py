1902ee721fd7ffa9393807d7cfc474d204360dcb
patil-suraj <surajp815@gmail.com>
2023-09-21 13:51:34 +0200

    up

--- muse/modeling_taming_vqgan.py
+++ muse/modeling_taming_vqgan.py
@@ -568,6 +568,7 @@ class VQGANModel(ModelMixin, ConfigMixin):
         dropout: float = 0.0,
         resample_with_conv: bool = True,
         commitment_cost: float = 0.25,
+        **kwargs,
     ):
         super().__init__()
 
