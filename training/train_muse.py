a9431c3481907bf4e607b7a06a2b6b1fd635a508
isamu-isozaki <isamu.website@gmail.com>
2023-11-11 21:19:48 -0500

    Ignore unused params

--- training/train_muse.py
+++ training/train_muse.py
@@ -259,7 +259,9 @@ def main():
         torch.backends.cudnn.deterministic = False
 
     config.experiment.logging_dir = str(Path(config.experiment.output_dir) / "logs")
-    ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+    # ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+    ddp_kwargs = DistributedDataParallelKwargs()
+
     accelerator = Accelerator(
         gradient_accumulation_steps=config.training.gradient_accumulation_steps,
         mixed_precision=config.training.mixed_precision,
